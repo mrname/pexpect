9a8da33e003192cd71bb918e87475da88964b230
Jeff Quast <jquast@rivermeadow.com>
2014-06-01 12:11:38 -0700

    assertIn fix for cygwin

--- tests/test_filedescriptor.py
+++ tests/test_filedescriptor.py
@@ -32,18 +32,20 @@ class ExpectTestCase(PexpectTestCase.PexpectTestCase):
         s = fdpexpect.fdspawn (fd)
         s.expect(b'This is the end of test data:')
         s.expect(pexpect.EOF)
-        self.assertIn(s.before, b' END\n',     # unix
-                                b' END\r\n',)  # cygwin)
+        self.assertIn(s.before, (b' END\n',     # unix
+                                 b' END\r\n',)  # cygwin)
+                      )
 
     def test_maxread (self):
         fd = os.open ('TESTDATA.txt', os.O_RDONLY)
         s = fdpexpect.fdspawn (fd)
         s.maxread = 100
         s.expect('2')
-        s.expect ('This is the end of test data:')
-        s.expect (pexpect.EOF)
-        self.assertIn(s.before, b' END\n',     # unix
-                                b' END\r\n',)  # cygwin)
+        s.expect('This is the end of test data:')
+        s.expect(pexpect.EOF)
+        self.assertIn(s.before, (b' END\n',     # unix
+                                 b' END\r\n',)  # cygwin)
+                      )
 
     def test_fd_isalive (self):
         fd = os.open ('TESTDATA.txt', os.O_RDONLY)
@@ -75,10 +77,3 @@ if __name__ == '__main__':
     unittest.main()
 
 suite = unittest.makeSuite(ExpectTestCase, 'test')
-
-#fout = open('delete_me_1','wb')
-#fout.write(the_old_way)
-#fout.close
-#fout = open('delete_me_2', 'wb')
-#fout.write(the_new_way)
-#fout.close
